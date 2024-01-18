c73ed18724301e58caf5f108368d0b48e704a735
Sathish-30 <sathish.amsaravanan@gmail.com>
2024-01-18 06:36:33 +0530

    Added the learning material for react hooks and initialized the currency converter project

---
+++ react/currency-converter/src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+    return (
+        <div className = "bg-cover bg-no-repeat w-full h-screen flex flex-wrap justify-center items-center" style={{background:`url(https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}}>
+            <h1 className= "bg-sky-200">Test for tailwindcss</h1>
+        </div>
+    )
+}
+export default App;
