abc7e83fc77750faa1eafebdba3bce51d1caad4e
Sathish-30 <sathish.amsaravanan@gmail.com>
2024-01-20 22:36:53 +0530

    Added the project initialization for the useContext

---
+++ react/contextApi/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
+
+const App = () => {
+  return (
+    <UserContextProvider>
+      <h1>React video for context api</h1>
+      <Login/>
+      <Profile/>
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ react/contextApi/src/components/Login.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState , useContext } from 'react';
+import UserContext from '../context/UserContext';
+const Login = () => {
+    const [userName , setUserName] = useState("");
+    const [password , setPassword] = useState("");
+
+    const {setUser} = useContext(UserContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setUser({userName , password});
+    }
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <input type="text" 
+                name="username"
+                value={userName} 
+                placeholder='Username'
+                onChange={(e) => setUserName(e.target.value)} />
+            {" "}
+            <input type="password" 
+                    name="password" 
+                    placeholder='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
+            <button type = "submit" onClick={handleSubmit}>Submit</button>
+        </div>
+    )
+}
+
+export default Login
